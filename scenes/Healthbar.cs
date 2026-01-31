430c0ba1a7164cc17bb84d73e37b114a1ec18d8c
Fabio Nogueira <fabiomiguel98@hotmail.com>
2026-01-31 20:25:31 +0000

    Mask Health

--- scenes/Healthbar.cs
+++ scenes/Healthbar.cs
@@ -4,60 +4,73 @@ public partial class Healthbar : ProgressBar
 {
 	private Timer timer;
 	private ProgressBar damageBar;
+	private TextureProgressBar maskBar;
 
-	private float health;
+	private float hostHealth;
+	private float maskHealth;
 
 	public override void _Ready()
 	{
 		AddToGroup("player_healthbar");
 		timer = GetNode<Timer>("Timer");
 		damageBar = GetNode<ProgressBar>("DamageBar");
+		maskBar = GetNode<TextureProgressBar>("TextureProgressBar");;
 
 		timer.Timeout += OnTimerTimeout;
 		InitHealth(100);
 	}
+	
+	public void SetMaskHealth(float newHealth) 
+	{
+		maskHealth = (float) Mathf.Min(maskBar.MaxValue, newHealth);
+		maskBar.Value = maskHealth;
+	}
 
 	public void SetHealth(float newHealth)
 	{
-		float prevHealth = health;
-		health = (float) Mathf.Min(MaxValue, newHealth);
+		float prevHealth = hostHealth;
+		hostHealth = (float) Mathf.Min(MaxValue, newHealth);
 
-		if (health < 0)
+		if (hostHealth < 0)
 		{
-			QueueFree();
+			//QueueFree();
 			return;
 		}
 
-		Value = health;
-
-		if (health < prevHealth)
+		Value = hostHealth;
+		
+		if (hostHealth < prevHealth)
 		{
 			timer.Start();
 		}
 		else
 		{
-			damageBar.Value = health;
+			damageBar.Value = hostHealth;
 		}
 	}
 	
-	public void DecreaseHealth(float newHealth) 
+	public void DecreaseHealth(float damage) 
 	{
-		SetHealth(health-newHealth);
+		SetHealth(hostHealth-damage);
+		SetMaskHealth(maskHealth-(float)(damage*0.1));
 	}
 
 	public void InitHealth(float initialHealth)
 	{
-		health = initialHealth;
+		hostHealth = initialHealth;
+		maskHealth = initialHealth;
 
 		MaxValue = initialHealth;
 		Value = initialHealth;
 
 		damageBar.MaxValue = initialHealth;
 		damageBar.Value = initialHealth;
+		maskBar.MaxValue = initialHealth;
+		maskBar.Value = initialHealth;
 	}
 
 	private void OnTimerTimeout()
 	{
-		damageBar.Value = health;
+		damageBar.Value = hostHealth;
 	}
 }
